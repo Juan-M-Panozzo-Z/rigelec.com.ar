2841d0d4f5c03bfc3e1f82fc1f404e3c641d540b
Juan-M-Panozzo-Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2023-07-05 17:32:49 -0300

    modified:   src/pages/nosotros.js

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -2,7 +2,7 @@ import IndexLayout from "./layouts/indexLayout";
 
 export default function NosotrosPage() {
     return (
-        <IndexLayout>
+        <IndexLayout title="Sobre nosotros">
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-6xl font-bold">Nosotros</h1>
             </div>
