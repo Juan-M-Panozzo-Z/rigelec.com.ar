99b7894ee4362166932e8243057b386a23826d7f
Juan-M-Panozzo-Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2023-07-05 16:44:30 -0300

    modified:   src/pages/components/Section.js modified:   src/pages/components/SectionTitle.js

--- src/pages/components/Section.js
+++ src/pages/components/Section.js
@@ -2,7 +2,7 @@ export default function Section({children}) {
     return (
         <section
         
-        className="container mx-auto px-12 mt-8 rounded-box space-y-2">
+        className="container mx-auto px-12 mt-8 rounded-box space-y-4">
             {children}
         </section>
     )
