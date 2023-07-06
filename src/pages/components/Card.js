7554114a6fb5ee0b7b096f252ab6252d20cc29e1
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-07-06 18:05:33 -0300

    modified:   src/pages/components/Card.js

--- src/pages/components/Card.js
+++ src/pages/components/Card.js
@@ -2,27 +2,35 @@ import Image from "next/image";
 
 export default function Card({ ...props }) {
     return (
-      <div className="card mx-8 bg-base-100 shadow-xl shadow-base-200 transition-all duration-200">
-        <figure>
-          <Image
-            src={props.image}
-            alt={props.title}
-            className="rounded-box aspect-square object-cover"
-            width={300}
-            height={400}
-          />
-        </figure>
-        <div className="flex flex-col justify-center gap-4 p-4">
-          <h2 className="card-title text-[10px] md:text-xs">{props.title.slice(0, 30)} [...]</h2>
-          <h3 className="card-subtitle text-[10px] md:text-xs">
-          Marca
-          <div className="badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">{props.subtitle}</div>
-          </h3>
-          <p className="card-description text-[10px] md:text-xs">
-          Modelo
-          <div className="badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">{props.description.slice(0,15)} [...]</div>
-          </p>
+        <div className="card mx-8 bg-base-100 shadow-xl shadow-base-200 transition-all duration-200">
+            <figure>
+                <Image
+                    src={props.image}
+                    alt={props.title}
+                    className="rounded-box aspect-square object-cover"
+                    width={300}
+                    height={400}
+                />
+            </figure>
+            <div className="flex flex-col justify-center gap-4 p-4">
+                <h2 className="card-title text-[10px] md:text-xs">
+                    {props.title ? props.title.slice(0, 30) + " [...]" : ""}
+                </h2>
+                <h3 className="card-subtitle text-[10px] md:text-xs">
+                    Marca
+                    <div className="badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">
+                        {props.subtitle}
+                    </div>
+                </h3>
+                <p className="card-description text-[10px] md:text-xs">
+                    Modelo
+                    <div className="badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">
+                        {props.description
+                            ? props.description.slice(0, 15) + " [...]"
+                            : ""}
+                    </div>
+                </p>
+            </div>
         </div>
-      </div>
     );
-  }
+}
