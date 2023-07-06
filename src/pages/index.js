98efe0e90fb598354789dca43c66eba0ef8d181e
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-07-06 18:45:33 -0300

    modified:   src/pages/_app.js modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,9 +54,7 @@ export default function Home() {
             <SectionFluid>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3405.958804991934!2d-58.0255689251823!3d-31.387699394863642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95ade810c013decd%3A0xed4b0d1cc11c9336!2sRIGELEC%20ELECTRICIDAD%20e%20ILUMINACI%C3%93N%20%2B%20ENERGIAS%20RENOVABLES!5e0!3m2!1sen!2sar!4v1688583605125!5m2!1sen!2sar"
-                    allowfullscreen=""
                     loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"
                     className="w-full h-96 rounded-box"
                 ></iframe>
             </SectionFluid>
