49a4c306be3a332f66543f50bab00701de21875b
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-07-07 17:11:21 -0300

    modified:   package-lock.json modified:   package.json modified:   src/pages/components/Card.js new file:   src/pages/components/Skeletons/Card.js modified:   src/pages/index.js

--- src/pages/components/Skeletons/Card.js
+++ src/pages/components/Skeletons/Card.js
@@ -1,34 +1,21 @@
-import Image from "next/image";
-
-export default function Card({ ...props }) {
+export default function SkeletonCard() {
     return (
         <div className="pb-16 md:pb-8">
             <div className="card mx-8 bg-base-100 shadow-xl shadow-base-200 transition-all duration-200">
-                <figure>
-                    <Image
-                        src={props.image}
-                        alt={props.title}
-                        className="rounded-box aspect-square object-cover"
-                        width={300}
-                        height={400}
-                    />
-                </figure>
                 <div className="flex flex-col justify-center gap-4 p-4">
                     <h2 className="card-title text-[10px] md:text-xs">
-                        {props.title ? props.title.slice(0, 30) + " [...]" : ""}
+                        Cargando...
                     </h2>
                     <h3 className="card-subtitle text-[10px] md:text-xs">
                         Marca
                         <div className="badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">
-                            {props.subtitle}
+                            Cargando...
                         </div>
                     </h3>
                     <p className="card-description text-[10px] md:text-xs">
                         Modelo
                         <div className="badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">
-                            {props.description
-                                ? props.description.slice(0, 15) + " [...]"
-                                : ""}
+                            Cargando...
                         </div>
                     </p>
                 </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Suspense } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./layouts/indexLayout";
 import Section from "./components/Section";
 import SectionFluid from "./components/SectionFluid";
@@ -9,13 +9,15 @@ import "slick-carousel/slick/slick-theme.css";
 import { useMediaQuery } from "@react-hook/media-query";
 import Card from "./components/Card";
 import axios from "axios";
+import SkeletonCard from "./components/Skeletons/Card";
 
 export default function Home() {
     const [articles, setArticles] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     const isMedium = useMediaQuery("only screen and (max-width: 768px)");
 
-
     useEffect(() => {
+        setIsLoading(true);
         axios
             .get("/api/ultimosIngresos")
             .then((response) => {
@@ -23,12 +25,38 @@ export default function Home() {
             })
             .catch((error) => {
                 console.log(error);
+            })
+            .finally(() => {
+                setIsLoading(false);
             });
     }, []);
 
+    const renderCards = () => {
+        if (isLoading) {
+          const skeletonCards = new Array(6).fill(0).map((_, index) => (
+            <SkeletonCard key={index} />
+          ));
+          return (
+            
+              skeletonCards
+            
+          );
+        } else {
+          return articles.map((article) => (
+            <Card
+              key={article.Id}
+              title={article.Descripcion}
+              subtitle={article.Marca}
+              description={`${article.Modelo} ${article.Medida}`}
+              image={article.ImageUrl}
+            />
+          ));
+        }
+      };
+
     return (
         <Layout title="Home">
-            <Section className={'px-8 md:px-12'}>
+            <Section className={"px-8 md:px-12"}>
                 <SectionTitle title="Ultimos ingresos" />
                 <Slider
                     dots={true}
@@ -38,19 +66,9 @@ export default function Home() {
                     slidesToScroll={isMedium ? 1 : 3}
                     className="w-full"
                 >
-                    {articles.map((article) => (
-                        <div key={article.ID} className="pb-16 md:pb-8">
-                            <Card
-                                title={article.Descripcion}
-                                subtitle={article.Marca}
-                                description={`${article.Modelo} ${article.Medida}`}
-                                image={article.ImageUrl}
-                            />
-                        </div>
-                    ))}
+                    {renderCards()}
                 </Slider>
             </Section>
-
             <SectionFluid>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3405.958804991934!2d-58.0255689251823!3d-31.387699394863642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95ade810c013decd%3A0xed4b0d1cc11c9336!2sRIGELEC%20ELECTRICIDAD%20e%20ILUMINACI%C3%93N%20%2B%20ENERGIAS%20RENOVABLES!5e0!3m2!1sen!2sar!4v1688583605125!5m2!1sen!2sar"
