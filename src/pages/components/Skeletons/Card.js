5393ef4356d5af3d79362cf13d81475b811d5fed
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-07-07 17:14:16 -0300

    modified:   src/pages/components/Skeletons/Card.js modified:   src/pages/index.js

--- src/pages/components/Skeletons/Card.js
+++ src/pages/components/Skeletons/Card.js
@@ -3,18 +3,18 @@ export default function SkeletonCard() {
         <div className="pb-16 md:pb-8">
             <div className="card mx-8 bg-base-100 shadow-xl shadow-base-200 transition-all duration-200">
                 <div className="flex flex-col justify-center gap-4 p-4">
-                    <h2 className="card-title text-[10px] md:text-xs">
+                    <h2 className="animate-pulse card-title text-[10px] md:text-xs">
                         Cargando...
                     </h2>
                     <h3 className="card-subtitle text-[10px] md:text-xs">
                         Marca
-                        <div className="badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">
+                        <div className="animate-pulse badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">
                             Cargando...
                         </div>
                     </h3>
                     <p className="card-description text-[10px] md:text-xs">
                         Modelo
-                        <div className="badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">
+                        <div className="animate-pulse badge badge-sm mx-2 badge-primary text-[10px] md:text-xs">
                             Cargando...
                         </div>
                     </p>
