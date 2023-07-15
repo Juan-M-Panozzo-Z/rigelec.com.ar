8b5b034fa373b0e72a241161ec8a348ad3ab0cb5
Juan-M-Panozzo-Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2023-07-15 12:07:22 -0300

    modified:   src/pages/api/ultimosIngresos.js modified:   src/pages/components/Card.jsx

--- src/pages/api/ultimosIngresos.js
+++ src/pages/api/ultimosIngresos.js
@@ -2,7 +2,8 @@ const Firebird = require("node-firebird");
 
 export default function handler(req, res) {
     const options = {
-        host: "201.252.141.39",
+        host: "rigelec.com.ar",
+        // host: "10.16.10.16",
         port: 3050,
         database: "D:\\ETSOL\\PaljetERP\\database\\DBSIF.FDB",
         user: "SYSDBA",
