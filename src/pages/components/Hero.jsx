c2c5819122037b887c70810cccd1b446b7fd7861
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-09-09 10:20:07 -0300

    deleted:    public/assets/hero/header-shape.svg new file:   public/assets/hero/hero.jpg modified:   src/pages/components/Hero.jsx

--- src/pages/components/Hero.jsx
+++ src/pages/components/Hero.jsx
@@ -4,15 +4,15 @@ export default function Hero() {
     return (
         <section>
             <Image
-                className="h-screen object-cover rounded-b-box opacity-90"
-                src="/assets/hero/1.jpg"
+                className="h-screen object-cover brightness-90 filter"
+                src="/assets/hero/hero.jpg"
                 width={1920}
                 height={1080}
                 alt="hero"
             />
             
             <div className="absolute -top-10 left-0 w-full h-full flex justify-center items-center">
-                <div className="text-center m-4 p-8 rounded-box text-white flex flex-col gap-4">
+                <div className="text-center m-4 text-white flex flex-col gap-4">
                     <h1 className="text-3xl md:text-8xl">
                         ¿Necesitas comprar algo?
                     </h1>
