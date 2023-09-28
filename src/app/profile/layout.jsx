9b29aa084c0269e2f0e70f67876ee7ddcfdfcbac
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-09-28 18:06:15 -0300

    modified:   src/app/profile/layout.jsx 	modified:   src/styles/globals.css

--- src/app/profile/layout.jsx
+++ src/app/profile/layout.jsx
@@ -6,14 +6,14 @@ const ProfileLayout = ({ children }) => (
         <div className="drawer-content">
             <label
                 htmlFor="my-drawer"
-                className="fixed left-2 inset-y-1/2 drawer-overlay btn btn-sm btn-circle btn-primary text-white"
+                className="fixed left-2 inset-y-1/2 drawer-overlay btn btn-sm btn-circle btn-primary text-white wiggle-animation"
             >
                 <GoArrowRight />
             </label>
             {children}
         </div>
         <div className="drawer-side">
-            <label htmlFor="my-drawer" className="drawer-overlay"/>
+            <label htmlFor="my-drawer" className="drawer-overlay" />
             <ul className="menu pt-24 p-4 w-56 min-h-full bg-base-200 text-base-content rounded-r-xl"></ul>
         </div>
     </div>
