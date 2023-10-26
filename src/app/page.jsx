61226b18a931855e07b1de6aaff3537feeabdb27
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-10-26 17:24:58 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/page.jsx 	new file:   src/components/builder.io/Noticies.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import SectionFluid from "@/components/SectionFluid";
 import ContactUs from "@/components/ContactUs";
 import Hero from "@/components/Hero";
 import UltimosMovimientos from "@/components/UltimosMovimientos";
+import Noticies from "@/components/builder.io/Noticies";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <UltimosMovimientos />
       <ContactUs />
+      <Noticies />
       <SectionFluid>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3405.958804991934!2d-58.0255689251823!3d-31.387699394863642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95ade810c013decd%3A0xed4b0d1cc11c9336!2sRIGELEC%20ELECTRICIDAD%20e%20ILUMINACI%C3%93N%20%2B%20ENERGIAS%20RENOVABLES!5e0!3m2!1sen!2sar!4v1688583605125!5m2!1sen!2sar"
