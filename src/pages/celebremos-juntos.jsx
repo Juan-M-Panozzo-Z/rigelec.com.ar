f95f862aa751c9bfbd17edfae35ced69af9248f1
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-11-13 11:44:02 -0300

    modificados:     package-lock.json 	nuevos archivos: src/pages/celebremos-juntos.jsx

---
+++ src/pages/celebremos-juntos.jsx
@@ -0,0 +1,13 @@
+export default function CelebremosJuntos() {
+    return (
+        <iframe
+            src="https://forms.gle/xyFrX7NB5cqAidv4A"
+            className="w-full h-screen"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+        >
+            Cargando…
+        </iframe>
+    );
+}
