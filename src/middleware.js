114ce39d0f0203976549b585c5b773807fa64818
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-11-29 12:05:55 -0300

    incorporacion de middleware + ssr

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,59 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 
-export async function middleware(req) {
-    const res = NextResponse.next()
-    const supabase = createMiddlewareClient({ req, res })
+export async function middleware(request) {
+    let response = NextResponse.next({
+        request: {
+            headers: request.headers,
+        },
+    })
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser()
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        {
+            cookies: {
+                get(name) {
+                    return request.cookies.get(name)?.value
+                },
+                set(name, value, options) {
+                    request.cookies.set({
+                        name,
+                        value,
+                        ...options,
+                    })
+                    response = NextResponse.next({
+                        request: {
+                            headers: request.headers,
+                        },
+                    })
+                },
+                remove(name, options) {
+                    request.cookies.set({
+                        name,
+                        value: '',
+                        ...options,
+                    })
+                    response = NextResponse.next({
+                        request: {
+                            headers: request.headers,
+                        },
+                    })
+                },
+            },
+        }
+    )
 
-    // if (!user && req.nextUrl.pathname.includes('/dashboard')) {
-    //     return NextResponse.redirect(new URL('/auth/login', req.url))
-    // }
+    // Obtener el usuario actual
+    const user = await supabase.auth.getSession()
 
-    return res
-}
+    // Obtener el path actual
+    const path = request.nextUrl.pathname
 
-export const config = {
-    matcher: ['/', '/dashboard'],
+    // Si el usuario no está autenticado o el path incluye /dashboard, redirigir a /auth/login
+    if (!user || path.includes('/dashboard')) {
+        response = NextResponse.redirect(new URL('/auth/login', request.url))
+    }
+
+    return response
 }
