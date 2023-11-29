cbccdb07f1802fa7da681dccc276561238b6cb83
Juan-M-Panozzo-Z <113150244+Juan-M-Panozzo-Z@users.noreply.github.com>
2023-11-29 10:37:21 -0300

    se limpió prisma u next-auth

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,4 @@
 import "@/styles/globals.css";
-import { NextAuthProvider } from "@/components/Provider";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import WhatsappButton from "@/components/WhatsappButton";
@@ -7,14 +6,12 @@ import WhatsappButton from "@/components/WhatsappButton";
 const RootLayout = ({ children }) => {
   return (
     <html lang="en" suppressHydrationWarning>
-      <NextAuthProvider>
         <body>
           <Navbar />
           <section className="min-h-screen">{children}</section>
           <WhatsappButton />
           <Footer />
         </body>
-      </NextAuthProvider>
     </html>
   );
 };
